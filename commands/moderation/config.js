3fd2c10de8bafb3d4b948730d9992b942df7b193
ABHINAV TOMAR <atomar.cse18@chitkarauniversity.edu.in>
2020-04-25 01:44:19 +0530

    Config module updates

--- commands/moderation/config.js
+++ commands/moderation/config.js
@@ -1,3 +1,6 @@
+const { setLogState } = require('../../helpers');
+const { log_state, vote_mute_threshold, vote_mute_roles} = require('../../server.json');
+
 module.exports = {
     name: 'config',
     category: 'moderation',
@@ -9,8 +12,59 @@ module.exports = {
         }
         else {
             console.log(args);
-            if (args==null||args[0]=="help"){
-                message.channel.send("help, logging, staff");
+            if (args[0]==null||args[0]=="help"){
+                message.channel.send("help, logging, votemute");
+            }
+            if (args[0]=="logging"){
+                if (args[1]==null||args.length>2){
+                    message.reply("Usage: config logging enable|disable");
+                }
+                else if (args[1]=='enable'||args[1]=='disable'||args[1]=='state'){
+                    var state;
+                    if (args[1]=='enable'){
+                        state = true;
+                        if (state==log_state){
+                            message.channel.send("Logging is already enabled");
+                            return;
+                        }
+                    }
+                    else if (args[1]=='disable'){
+                        state = false;
+                        if (state==log_state){ 
+                            message.channel.send("Logging is already disabled");
+                            return;
+                        }
+                    }
+                    else if (args[1]=='state'){
+                        message.channel.send(`Logging is set to ${log_state}`);
+                        return;
+                    }
+                    //setLogState(state);
+                    message.channel.send("Logging " + args[1] + "d");
+                    console.log(log_state);
+                }
+            }
+            else if (args[0]=="votemute"){
+                if (args[1]==null){
+                    message.channel.send("Usage: config votemute threshold|roleset");
+                }
+                else if (args[1]=="threshold"){
+                    if (args[2]==null){
+                        message.channel.send(`**Current Threshold:**  ${vote_mute_threshold}`)
+                    }
+                    else if (args[2]>=3){
+                        //setMuteThreshold(args[1]);
+                        message.channel.send(`Threshold Updated to ${args[2]}`);
+                    }
+                    else {
+                        message.channel.send(`Please use a valid integer above 3`);
+                    }
+                }
+                else if (args[2]=="roles"){
+                    if (args[3]=="add"){
+                        //TODO: USE GET ROLE FUNCTION TO APPEND ROLE TO ARRAY AND RE DO HELPER FUNCTIONS
+                    }
+                }
             }
         }
     }
