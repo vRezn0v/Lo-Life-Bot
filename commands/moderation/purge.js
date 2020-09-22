c34d83f71263ec2f1e5a0d34cb0e757b0e7ea06e
Abhinav <34809845+vRezn0v@users.noreply.github.com>
2020-09-22 18:13:46 +0530

    Purge V12 Rebase

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -1,4 +1,4 @@
-const { RichEmbed } = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 const { stripIndents } = require("common-tags");
 
 const { isModerator } = require('../../helpers');
@@ -9,29 +9,22 @@ module.exports = {
     usage: `**Requires Elevated Access**\`\`\`purge <number of messages>\`\`\``,
     run: async(client, message, args) => {
         if (isModerator(message)){
+            const channel = message.channel;
             const user = message.mentions.users.first();
             const amount = !!parseInt(args[0]) ? parseInt(args[0]) : parseInt(args[1]);
             console.log(user + "\n" + amount);
-            message.channel.fetchMessages({
-                limit: 100,
-               }).then((messages) => {
-                if (user) {
-                    const filterBy = user ? user.id : Client.user.id;
-                    messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount+1);
-                } else {
-                    messages = messages.array().slice(0, amount+1);
-                }
-                const embed = new RichEmbed()
+            channel.bulkDelete(amount+1)
+            .then(messages => {
+                const embed = new MessageEmbed()
                 .setColor("#001a00")
                 .setTimestamp()
                 .setFooter(message.guild.name, message.guild.iconURL)
                 .setAuthor("def not Rez in disguise")
                 .setDescription(stripIndents`**Purged:** ${amount} messages.`);
-                const channel = message.channel;
-                message.delete();
-                channel.bulkDelete(messages).catch(error => console.log(error.stack));
-                channel.send(embed).then(m => m.delete(10000));
-            });
+                channel.send(embed).then(m => m.delete({ timeout: 5000 }));
+                console.log(`Bulk deleted ${messages.size} messages`)
+            })
+            .catch(console.error);
         } else {
             message.reply("Unauthorized! Find you and feed you cookies with water I must.");
         }
