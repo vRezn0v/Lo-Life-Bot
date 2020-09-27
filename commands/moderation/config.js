83441e3b9ca3ae70c9e13cd724462ea91ab01a36
Abhinav <34809845+vRezn0v@users.noreply.github.com>
2020-09-27 19:06:40 +0530

    Overview Embed Added

--- commands/moderation/config.js
+++ commands/moderation/config.js
@@ -1,5 +1,6 @@
+const { MessageEmbed } = require('discord.js');
 const { setLogState, authorIsAdmin } = require('../../helpers');
-const { log_state, vote_mute_threshold, vote_mute_roles } = require('../../server.json');
+const { log_state, vote_mute_threshold, server_prefix } = require('../../server.json');
 
 module.exports = {
     name: 'config',
@@ -12,7 +13,15 @@ module.exports = {
             message.reply("You are not authorized to perform this action.");
         }
         else {
-            if (args[0]=="logging"){
+            if (args[0]=="show") {
+                const embed = new MessageEmbed()
+                                .setTitle(`${message.guild.name} Config`)
+                                .setDescription(`**Prefix:** ${server_prefix}\n**Vote Threshold:** ${vote_mute_threshold}\n**Logging Status:** ${log_state}`)
+                                .setTimestamp()
+                                .setFooter("Brought To You By Raid Shadow Legends");
+                return message.channel.send(embed);
+            }
+            else if (args[0]=="logging"){
                 if (args[1]==null||args.length>2){
                     message.reply("Usage: config logging <enable|disable>");
                 }
@@ -46,15 +55,15 @@ module.exports = {
                     message.channel.send("Usage: config votemute <threshold n|roleset rolename>");
                 }
                 else if (args[1]=="threshold"){
-                    if (args[2]==null){
-                        message.channel.send(`**Current Threshold:**  ${vote_mute_threshold}`)
+                    if (args[2]==null||args[2]=='status'){
+                        message.channel.send(`**Current Vote Threshold:**  ${vote_mute_threshold}`);
                     }
                     else if (args[2]>=3){
                         //setMuteThreshold(args[1]);
                         message.channel.send(`Threshold Updated to ${args[2]}`);
                     }
                     else {
-                        message.channel.send(`Please use a valid integer above 3`);
+                        message.channel.send(`Please use a valid number above 3`);
                     }
                 }
                 else if (args[2]=="powerroles"){
