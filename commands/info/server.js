86ff8e0bbb5ae90da41bf21553126ebd88fff3c5
Abhinav <34809845+vRezn0v@users.noreply.github.com>
2020-09-27 19:06:30 +0530

    Switch To MessageEmbeds

--- commands/info/server.js
+++ commands/info/server.js
@@ -1,3 +1,5 @@
+const { MessageEmbed } = require("discord.js");
+
 module.exports = {
     name: 'server',
     usage: 'server',
@@ -6,48 +8,35 @@ module.exports = {
         const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
         var userCount = message.guild.members.cache.filter(member => !member.user.bot).size;
         var botCount = message.guild.members.cache.filter(member => member.user.bot).size; 
-        const serverinfo = {
-            color: 0xFF1493,
-            title: message.guild.name,
-            description: '**Server Details**',
-            thumbnail: {
-                url: message.guild.iconURL,
-            },
-            fields: [
-                {
-                    name: 'Created At:',
-                    value: '```' + message.guild.createdAt.getDate() + ' ' + month[message.guild.createdAt.getMonth()] + ' ' + message.guild.createdAt.getFullYear() + '```',
-                    inline: false,
-                },
-                {
-                    name: 'Server ID:',
-                    value: '```' + message.guild.id + '```',
-                    inline: true,
-                },
-                {
-                    name: 'Owner:',
-                    value: '```' + message.guild.owner.user.username + '#' + message.guild.owner.user.discriminator + '```',
-                    inline: true,
-                },
-                {
-                    name: 'Member Count: ' + message.guild.members.size,
-                    value: '```' + userCount+' humans'+' & '+botCount+' bots' + '```',
-                    inline: false,
-                },
-                {
-                    name: '\u200b',
-                    value: '\u200b',
-                    inline: false,
-                },
-            ],
-            
-            timestamp: new Date(),
-            footer: {
-                text: 'powered by Anti Rebellion',
-            },
-        };
+        const serverinfo = new MessageEmbed()
+            .setColor('0xFF1493')
+            .setTitle(message.guild.name)
+            .setDescription(`**Server Details**`)
+            .setThumbnail(message.guild.iconURL({ format: 'png', dynamic: true, size: 1024 }))
+            .addFields({
+                name: 'Created At:',
+                value: '```' + message.guild.createdAt.getDate() + ' ' + month[message.guild.createdAt.getMonth()] + ' ' + message.guild.createdAt.getFullYear() + '```',
+                inline: false
+            }, {
+                name: 'Server ID:',
+                value: '```' + message.guild.id + '```',
+                inline: true
+            }, {
+                name: 'Owner:',
+                value: '```' + message.guild.owner.user.username + '#' + message.guild.owner.user.discriminator + '```',
+                inline: true
+            }, {
+                name: 'Member Count: ' + message.guild.members.cache.size,
+                value: '```' + userCount+' humans'+' & '+botCount+' bots' + '```',
+                inline: false
+            }, {
+                name: `Region`,
+                value: '```' + message.guild.region.charAt(0).toUpperCase() + message.guild.region.slice(1) + '```'
+            })
+            .setTimestamp()
+            .setFooter("Powered By Tea");
         
-        message.channel.send({ embed: serverinfo });
+        message.channel.send(serverinfo);
         
     }
 }
