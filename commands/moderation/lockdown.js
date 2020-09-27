f19e2fc3fbdb47b5d9a22c17d512b8901a5569ce
Abhinav <34809845+vRezn0v@users.noreply.github.com>
2020-09-27 23:29:35 +0530

    Boilerplate Code for Lock and Unlock Channel

---
+++ commands/moderation/lockdown.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'lockdown',
+    description: 'Locks Down A Channel, blocking texts from everyone below Mod Access (To Be Used From Within The Channel To Be Locked)',
+    usage: `**Requires Elevated Access**\`\`\`lockdown\`\`\``,
+    run: async (client, message, args) => {
+        // LOCKDOWN FLOW
+    }
+}
---
+++ commands/moderation/unlock.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'unlock',
+    description: 'Unlocks A Previously Locked Channel (To Be Used From Within The Channel To Be Locked)',
+    usage: `**Requires Elevated Access**\`\`\`unlock\`\`\``,
+    run: async (client, message, args) => {
+        // Unlock Flow Here
+    }
+}
