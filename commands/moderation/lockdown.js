eae309770ec6d0e169690e656498be4545709eba
Abhinav <34809845+vRezn0v@users.noreply.github.com>
2020-10-02 23:37:15 +0530

    New Category

--- commands/moderation/lockdown.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'lockdown',
-    description: 'Locks Down A Channel, blocking texts from everyone below Mod Access (To Be Used From Within The Channel To Be Locked)',
-    usage: `**Requires Elevated Access**\`\`\`lockdown\`\`\``,
-    run: async (client, message, args) => {
-        // LOCKDOWN FLOW
-    }
-}
