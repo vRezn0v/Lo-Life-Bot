bb77728ce9689d6bf8ae96664d92a3854281e51c
Abhinav <34809845+vRezn0v@users.noreply.github.com>
2020-10-04 00:49:31 +0530

    Showrole Embed Created

--- commands/rolemanager/showroles.js
+++ commands/rolemanager/showroles.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require('discord.js');
 const { getMember } = require('../../helpers');
 
 module.exports = {
@@ -6,11 +7,25 @@ module.exports = {
     run: async (client, message, args) => {
         const roles = new Map();
         if (args.length===0||args[0]=='all') {
+            let lsr = [];
             message.guild.roles.cache.forEach(e => roles.set(e.name, e.id)) || "none";
+            for (let [key, value] of roles.entries()) {
+                lsr.push("**"+key+"**\t");
+                lsr.push("\`\`\`");
+                lsr.push(value);
+                lsr.push("\`\`\`\n\n");
+                //console.log(key + " " + "\`\`\`" + value + "\`\`\`");
+            }
+
+            lsr = lsr.join("");
+            const embed = new MessageEmbed()
+                            .setTitle("Roles List")
+                            .setDescription(lsr)
+                            .setColor("#0000FF");
+            return message.channel.send(embed);
         } else {
             target = getMember(message);
             target.roles.cache.forEach(e => roles.set(e.name, e.id)) || "none";
         }
-        console.log(roles);
     }
 }
