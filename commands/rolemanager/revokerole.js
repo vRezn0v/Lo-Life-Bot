66a0d0da0ede24859f1c59e5fe289ff6523523ef
Abhinav <34809845+vRezn0v@users.noreply.github.com>
2020-10-05 23:18:45 +0530

    Initialize Role Remover

---
+++ commands/rolemanager/revokerole.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'revokerole',
+    usage: '**Elevated Access Required**\n\`\`\`revokerole <@user> <roleid>\`\`\`',
+    category: 'rolemanager',
+    description: "Removes An ID specified role from the mentioned user.",
+    run: async (client, message, args) => {
+        
+    }
+}
